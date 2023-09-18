1a9920d7ab7a2df109bddce7f8869cad5182fea2
Mihail <lepadatu.mihail1@gmail.com>
2023-09-18 15:18:49 +0200

    fix: Add twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import '@/styles/tailwind.css'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
+  title: {
+    template: '%s - Work Arena',
+    default: 'Work Arena - Job offers made simple for everybody',
+  },
+  description:
+    'Find a perfect job with Work Arena in an always thriving industry withing the Dutch companies.',
+
   openGraph: {
     title: {
       template: '%s - Work Arena',
@@ -16,21 +23,21 @@ export const metadata: Metadata = {
     url: 'https://workarena.nl',
     siteName: 'WorkArena.nl',
     images: [
-      {
-        url: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
-        width: 900,
-        height: 400,
-      },
+      'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
     ],
     locale: 'en_US',
     type: 'website',
   },
-  title: {
-    template: '%s - Work Arena',
-    default: 'Work Arena - Job offers made simple for everybody',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'workarena.nl',
+    description:
+      'Find a perfect job with Work Arena in an always thriving industry withing the Dutch companies.',
+    creator: '@workarena.nl',
+    images: [
+      'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
+    ],
   },
-  description:
-    'Find a perfect job with Work Arena in an always thriving industry withing the Dutch companies.',
 }
 
 const inter = Inter({
