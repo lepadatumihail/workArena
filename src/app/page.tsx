584cf2b046f96443f125bd39ab11b6d17003fdfc
Mihail <lepadatu.mihail1@gmail.com>
2023-09-28 20:49:52 +0200

    fox:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Hero />
         <SectionList />
         {/* <PrimaryFeatures /> */}
-        <SecondaryFeatures />
+        {/* <SecondaryFeatures /> */}
         <CallToAction />
         <Example />
         <Testimonials />
